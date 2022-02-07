b005d0278212667ff3d9227d85ec1111985f93fe
satvikgoyal <satvik0354.cse19@chitkara.edu.in>
2022-02-08 00:05:55 +0530

    sample commit

---
+++ snake game/p.js
@@ -0,0 +1,17 @@
+
+<html>
+<head>
+    <title>Rohan</title>
+    <script type="text/javascript">
+    function show()
+        {
+             document.getElementById("one").innerHTML="You Tube";
+        }
+    </script>
+    </head>
+    <body>
+           <p onmouseover="show()"> <a href="https://www.youtube.com/"></a> </p>
+    <p id="one"></p>
+     
+    </body>
+</html>
